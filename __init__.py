b565439f3d30cf894024c0d75a3283bd31cf44cb
xxai.art <xxai.art@gmail.com>
2023-04-26 17:44:24 +0800

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+
+from .private_detector.utils.preprocess import preprocess_for_evaluation  # noqa: F401
--- inference.py
+++ inference.py
@@ -8,7 +8,7 @@ from private_detector.utils.preprocess import preprocess_for_evaluation
 
 
 def read_image(filename: str) -> tf.Tensor:
-    """
+  """
     Load and preprocess image for inference with the Private Detector
 
     Parameters
@@ -21,22 +21,18 @@ def read_image(filename: str) -> tf.Tensor:
     image : tf.Tensor
         Image ready for inference
     """
-    image = tf.io.read_file(filename)
-    image = tf.io.decode_jpeg(image, channels=3)
+  image = tf.io.read_file(filename)
+  image = tf.io.decode_jpeg(image, channels=3)
 
-    image = preprocess_for_evaluation(
-        image,
-        480,
-        tf.float16
-    )
+  image = preprocess_for_evaluation(image, 480, tf.float16)
 
-    image = tf.reshape(image, -1)
+  image = tf.reshape(image, -1)
 
-    return image
+  return image
 
 
-def inference(model: str , image_paths: List[str]) -> None:
-    """
+def inference(model: str, image_paths: List[str]) -> None:
+  """
     Get predictions with a Private Detector model
 
     Parameters
@@ -46,36 +42,34 @@ def inference(model: str , image_paths: List[str]) -> None:
     image_paths : List[str]
         Path(s) to image to be predicted on
     """
-    model = tf.saved_model.load(model)
+  model = tf.saved_model.load(model)
 
-    for image_path in image_paths:
-        image = read_image(image_path)
+  for image_path in image_paths:
+    image = read_image(image_path)
 
-        preds = model([image])
+    preds = model([image])
 
-        print(f'Probability: {100 * tf.get_static_value(preds[0])[0]:.2f}% - {image_path}')
+    print(
+        f'Probability: {100 * tf.get_static_value(preds[0])[0]:.2f}% - {image_path}'
+    )
 
 
 if __name__ == '__main__':
-    tf.get_logger().setLevel('ERROR')
-    absl_logging.set_verbosity(absl_logging.ERROR)
+  tf.get_logger().setLevel('ERROR')
+  absl_logging.set_verbosity(absl_logging.ERROR)
 
-    parser = argparse.ArgumentParser()
+  parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        '--model',
-        type=str,
-        required=True,
-        help='Location of SavedModel to load'
-    )
+  parser.add_argument('--model',
+                      type=str,
+                      required=True,
+                      help='Location of SavedModel to load')
 
-    parser.add_argument(
-        '--image_paths',
-        type=str,
-        nargs='+',
-        required=True,
-        help='Paths to image paths to predict for'
-    )
+  parser.add_argument('--image_paths',
+                      type=str,
+                      nargs='+',
+                      required=True,
+                      help='Paths to image paths to predict for')
 
-    args = parser.parse_args()
-    inference(**vars(args))
+  args = parser.parse_args()
+  inference(**vars(args))
